1098eef0eddc23964ae161807f0cfbc48284674d
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-07-06 11:51:56 +0200

    Adding initial work (#2)
    * Adding initial work
    * Improving the example
    * Minor improvement
    * Adding examples

--- README.md
+++ README.md
@@ -3,9 +3,26 @@
 ## How to build in local
 
 ```bash
+sdk env install
+./mvnw clean spring-boot:run
 ./mvnw clean verify
+./mvnw clean test -Dtest=MyControllerE2ETest
+
+//Surefire report
+./mvnw clean test -Dtest=MyControllerE2ETest surefire-report:report
+jwebserver -p 9000 -d "$(pwd)/target/site/"
 ```
 
+## Results
+
+```bash
+should_work_sequential_execution	1.643 s
+should_work_executor	            0.050 s
+should_work_completable	            0.032 s
+should_work_gatherers	            0.065 s
+should_work_structural	            0.031 s
+should_work_structural_multiple	    0.023 s
+```
 
 ## Spring Boot CLI
 
---
+++ src/test/java/info/jab/ms/controller/MyControllerE2ETest.java
@@ -0,0 +1,166 @@
+package info.jab.ms.controller;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+//@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(OrderAnnotation.class)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, 
+    properties = {
+        "greek-gods-api-url=http://localhost:8089/greek",
+        "roman-gods-api-url=http://localhost:8089/roman",
+        "nordic-gods-api-url=http://localhost:8089/nordic"
+    })
+public class MyControllerE2ETest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    int randomServerPort;
+
+    WireMockServer wireMockServer;
+
+    @BeforeEach
+    public void setup() {
+        wireMockServer = new WireMockServer(8089);
+        wireMockServer.start();
+
+        wireMockServer.stubFor(get(urlEqualTo("/greek"))
+            .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .withBodyFile("greek-gods.json")));
+
+        wireMockServer.stubFor(get(urlEqualTo("/roman"))
+            .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .withBodyFile("roman-gods.json")));
+
+        wireMockServer.stubFor(get(urlEqualTo("/nordic"))
+            .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .withBodyFile("nordic-gods.json")));
+    }
+
+    @AfterEach
+    public void teardown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    @Order(1)
+    public void should_work_sequential_execution() throws Exception {
+        final String baseUrl = "http://localhost:" + randomServerPort + "/v1/gods-sequential";
+
+        ResponseEntity<List<String>> gods = this.restTemplate.exchange(
+            baseUrl,
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<>() {});
+            
+        assertThat(gods.getBody()).isNotNull();
+        assertThat(gods.getBody().size()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(2)
+    public void should_work_executor() throws Exception {
+        final String baseUrl = "http://localhost:" + randomServerPort + "/v1/gods-executor";
+
+        ResponseEntity<List<String>> gods = this.restTemplate.exchange(
+            baseUrl,
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<>() {});
+            
+        assertThat(gods.getBody()).isNotNull();
+        assertThat(gods.getBody().size()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(3)
+    public void should_work_completable() throws Exception {
+        final String baseUrl = "http://localhost:" + randomServerPort + "/v1/gods-completable";
+
+        ResponseEntity<List<String>> gods = this.restTemplate.exchange(
+            baseUrl,
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<>() {});
+            
+        assertThat(gods.getBody()).isNotNull();
+        assertThat(gods.getBody().size()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(4)
+    public void should_work_gatherers() throws Exception {
+        final String baseUrl = "http://localhost:" + randomServerPort + "/v1/gods-gatherers";
+
+        ResponseEntity<List<String>> gods = this.restTemplate.exchange(
+            baseUrl,
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<>() {});
+            
+        assertThat(gods.getBody()).isNotNull();
+        assertThat(gods.getBody().size()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(5)
+    public void should_work_structural() throws Exception {
+        final String baseUrl = "http://localhost:" + randomServerPort + "/v1/gods-structural";
+
+        ResponseEntity<List<String>> gods = this.restTemplate.exchange(
+            baseUrl,
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<>() {});
+            
+        assertThat(gods.getBody()).isNotNull();
+        assertThat(gods.getBody().size()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(6)
+    public void should_work_structural_multiple() throws Exception {
+        final String baseUrl = "http://localhost:" + randomServerPort + "/v1/gods-structural-multiple";
+
+        ResponseEntity<List<String>> gods = this.restTemplate.exchange(
+            baseUrl,
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<>() {});
+            
+        assertThat(gods.getBody()).isNotNull();
+        assertThat(gods.getBody().size()).isGreaterThan(0);
+    }
+}
